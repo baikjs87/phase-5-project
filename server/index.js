21f075ae85bbfdfd94b795935f21556c5da3a0c8
Jason Baik <baikjs87@gmail.com>
2022-10-14 02:25:05 -0400

    WIP ImageKit.io

---
+++ server/index.js
@@ -0,0 +1,31 @@
+/* 
+  This is our backend server.
+  Replace YOUR_IMAGEKIT_URL_ENDPOINT, YOUR_IMAGEKIT_PUBLIC_KEY, 
+  and YOUR_IMAGEKIT_PRIVATE_KEY with actual values
+*/
+const express = require('express');
+const app = express();
+const ImageKit = require('imagekit');
+
+const imagekit = new ImageKit({
+  urlEndpoint: ENV['end_point'],
+  publicKey: ENV['public_key'],
+  privateKey: ENV['private_key']
+});
+
+// allow cross-origin requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/auth', function (req, res) {
+  var result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
+app.listen(3001, function () {
+  console.log('Live at Port 3001');
+});
