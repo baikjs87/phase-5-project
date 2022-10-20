edd4fdafffd5106b71bbfee94c913d9b11d0f14f
Jason Baik <baikjs87@gmail.com>
2022-10-20 02:07:01 -0400

    deloying

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production? 
-    Rails.application.config.session_store :cookie_store, :key=> 'backend-imagekit', domain: *, tld_length: 2
+    Rails.application.config.session_store :cookie_store, :key=> 'backend-imagekit', tld_length: 2
 else
     Rails.application.config.session_store :cookie_store, :key=> 'backend-imagekit'
 end
