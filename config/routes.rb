9b3011db5eb49e6bab5b9587d76568f7c72c851a
Jason Baik <baikjs87@gmail.com>
2022-10-21 01:42:16 -0400

    trying to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
   post "/signup",  to: "users#create"
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
@@ -14,4 +10,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :images
   
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
